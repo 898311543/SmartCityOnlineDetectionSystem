92453fd51d72a21dfd304c8e93c0b8b6e21f076f
shubin <898311543@qq.com>
2018-02-03 17:45:28 +0800

    commit

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -5,7 +5,7 @@ return array(
         'DB_HOST'   => '118.89.244.53', // 服务器地址
         'DB_NAME'   => 'envireoment_detection', // 数据库名
         'DB_USER'   => 'root', // 用户名
-        'DB_PWD'    => 'shubin', // 密码
+        'DB_PWD'    => 'passwd', // 密码
         'DB_PORT'   => 3306, // 端口
         'DB_PREFIX' => '', // 数据库表前缀
         'TMPL_PARSE_STRING'=>array(
